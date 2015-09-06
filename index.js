8e6f412ee040bcfe72d809a96933c4f0ca679f18
Filipe Pinheiro <fampinh3iro@gmail.com>
2015-09-06 21:16:36 +0100

    Split all the things

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-var Skyscanner = require('skyscanner')
+var skyscanner = require('./lib/skyscanner')
 var moment = require('moment')
 var async = require('async')
+var range = require('lodash.range')
+var flatten = require('lodash.flatten')
 
-var MAX_PRICE = 60
-var WEEKS = 10
+var MAX_PRICE = 400
+var WEEKS = 3
+var FORMAT = 'YYYY-MM-DD'
+var PAIRS = [
+  { from: 5, to: 8 },
+  { from: 5, to: 7 },
+  { from: 6, to: 8 },
+  { from: 6, to: 7 }
+]
 
-var s = new Skyscanner({
-  country: 'PT',
-  currency: 'EUR'
+var options = flatten(mapOptionsToWeeks())
+
+function mapOptionsToWeeks () {
+  return range(WEEKS).map(function (w) {
+    return PAIRS.map(function (p) {
+      return {
+        returnDate: moment().day(p.to).add(w, 'week').format(FORMAT),
+        departureDate: moment().day(p.from).add(w, 'week').format(FORMAT),
+        sortType: 'price_alphabetic'
+      }
+    })
+  })
+}
+
+async.map(options, getQuotes, function (err, data) {
+  if (err) {
+    throw err
+  }
+  console.log(data);
 })
+
+function getQuotes(options, cb) {
+  skyscanner.destinations('LIS', 'EVERYWHERE', options).then(function (data) {
+
+    console.log(data)
+  });
+}
+/*
 
 async.times(WEEKS, function (n, next) {
   var opts = []
@@ -46,81 +79,84 @@ async.times(WEEKS, function (n, next) {
 
 function getQuotes (opts, cb) {
   s.destinations('LIS', 'EVERYWHERE', opts).then(function (data) {
-    var cheapRoutes = []
-    var cheapPlaces = []
-    var filteredData = {}
 
-    var mappedData = {
-      routes: {},
-      places: {},
-      quotes: {}
-    }
+    console.log(data)
 
-    filteredData.quotes = data.Quotes.filter(function (q) {
-      var isCheap = q.Price < MAX_PRICE && q.Outbound_ToStationId !== 'OPO'
-      if (isCheap) {
-        cheapRoutes.push(q.RouteId)
-      }
-      return isCheap
-    })
-
-    filteredData.routes = data.Routes.filter(function (r) {
-      var isCheap = cheapRoutes.indexOf(r.RouteId) !== -1
-      if (isCheap) {
-        cheapPlaces.push(r.DestinationId)
-        mappedData.routes[r.RouteId] = r
-      }
-      return isCheap
-    })
-
-    filteredData.places = data.Places.filter(function (p) {
-      var isCheap = cheapPlaces.indexOf(p.PlaceId) !== -1
-      if (isCheap) {
-        mappedData.places[p.PlaceId] = p
-      }
-      return isCheap
-    })
-
-    for (var i = 0; i < filteredData.quotes.length; i++) {
-      var quote = filteredData.quotes[i]
-      // var route = mappedData.routes[quote.RouteId]
-      var route = filteredData.routes.filter(function (r) {
-        return r.RouteId === quote.RouteId
-      })[0]
-
-      route.destination = mappedData.places[route.DestinationId]
-
-      route.quotes = route.quotes || []
-      route.quotes.push(quote)
-    }
-
-    // console.log('destinations', {
-    //   routes: data.Routes,
-    //   quotes: data.Quotes,
-    //   // places: data.Places,
-    //   // carriers: data.Carriers,
-    //   // agents: data.Agents,
-    // })
-
-    var stuffThatMatters = filteredData.routes.map(function (r) {
-      return {
-        price: r.quotes[0].Price,
-        destination: r.destination,
-        departure: moment(r.quotes[0].Outbound_DepartureDate).format('dddd, MMMM Do YYYY'),
-        return: moment(r.quotes[0].Inbound_DepartureDate).format('dddd, MMMM Do YYYY'),
-        // quotes: r.quotes,
-        url: 'http://www.skyscanner.pt/transport/flights/' + r.OriginId + '/' + r.DestinationId + '/' + opts.departureDate + '/' + opts.returnDate + '/'
-      }
-    })
-
-    if (stuffThatMatters.length > 0) {
-      console.log(JSON.stringify(stuffThatMatters, null, 2))
-    }
-
-    cb(null, filteredData.routes)
-  }).error(function (err) {
-    console.log('error', err)
-  })
+  //   var cheapRoutes = []
+  //   var cheapPlaces = []
+  //   var filteredData = {}
+  //
+  //   var mappedData = {
+  //     routes: {},
+  //     places: {},
+  //     quotes: {}
+  //   }
+  //
+  //   filteredData.quotes = data.Quotes.filter(function (q) {
+  //     var isCheap = q.Price < MAX_PRICE && q.Outbound_ToStationId !== 'OPO'
+  //     if (isCheap) {
+  //       cheapRoutes.push(q.RouteId)
+  //     }
+  //     return isCheap
+  //   })
+  //
+  //   filteredData.routes = data.Routes.filter(function (r) {
+  //     var isCheap = cheapRoutes.indexOf(r.RouteId) !== -1
+  //     if (isCheap) {
+  //       cheapPlaces.push(r.DestinationId)
+  //       mappedData.routes[r.RouteId] = r
+  //     }
+  //     return isCheap
+  //   })
+  //
+  //   filteredData.places = data.Places.filter(function (p) {
+  //     var isCheap = cheapPlaces.indexOf(p.PlaceId) !== -1
+  //     if (isCheap) {
+  //       mappedData.places[p.PlaceId] = p
+  //     }
+  //     return isCheap
+  //   })
+  //
+  //   for (var i = 0; i < filteredData.quotes.length; i++) {
+  //     var quote = filteredData.quotes[i]
+  //     // var route = mappedData.routes[quote.RouteId]
+  //     var route = filteredData.routes.filter(function (r) {
+  //       return r.RouteId === quote.RouteId
+  //     })[0]
+  //
+  //     route.destination = mappedData.places[route.DestinationId]
+  //
+  //     route.quotes = route.quotes || []
+  //     route.quotes.push(quote)
+  //   }
+  //
+  //   // console.log('destinations', {
+  //   //   routes: data.Routes,
+  //   //   quotes: data.Quotes,
+  //   //   // places: data.Places,
+  //   //   // carriers: data.Carriers,
+  //   //   // agents: data.Agents,
+  //   // })
+  //
+  //   var stuffThatMatters = filteredData.routes.map(function (r) {
+  //     return {
+  //       price: r.quotes[0].Price,
+  //       destination: r.destination,
+  //       departure: moment(r.quotes[0].Outbound_DepartureDate).format('dddd, MMMM Do YYYY'),
+  //       return: moment(r.quotes[0].Inbound_DepartureDate).format('dddd, MMMM Do YYYY'),
+  //       // quotes: r.quotes,
+  //       url: 'http://www.skyscanner.pt/transport/flights/' + r.OriginId + '/' + r.DestinationId + '/' + opts.departureDate + '/' + opts.returnDate + '/'
+  //     }
+  //   })
+  //
+  //   if (stuffThatMatters.length > 0) {
+  //     console.log(JSON.stringify(stuffThatMatters, null, 2))
+  //   }
+  //
+  //   cb(null, filteredData.routes)
+  // }).error(function (err) {
+  //   console.log('error', err)
+  // })
 }
 
 // s.calendar('DFWA', 'LHR').then(function(data) {
@@ -130,3 +166,4 @@ function getQuotes (opts, cb) {
 // s.autosuggest('lisboa').then(function(data) {
 //   // console.log('autosuggest', data)
 // })
+*/
