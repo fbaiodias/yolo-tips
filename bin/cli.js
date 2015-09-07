613a9b9a9ca7eae1286656a3c672d2bdb71681f9
Filipe Pinheiro <fampinh3iro@gmail.com>
2015-09-07 01:06:03 +0100

    Use arguments to specify weeks, origin and destination

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,15 @@ var goyolo = require('../lib')
 if (args instanceof Error) {
   console.error(args.message)
 } else {
-  goyolo()
+  var options = {
+    weeks: args.weeks,
+    origin: args.origin.toUpperCase(),
+    destination: args.destination.toUpperCase()
+  }
+  goyolo(options, function (err, data) {
+    if (err) {
+      throw err
+    }
+    console.log(JSON.stringify(data, null, 2))
+  })
 }
